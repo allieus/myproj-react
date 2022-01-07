405a8885a1f9f2057730bd9057902c5b49a7f736
Chinseok Lee <me@askcompany.kr>
2022-01-07 16:35:56 +0900

    Review Delete API를 활용하여 리뷰 삭제를 구현

--- src/pages/reviews/ReviewList.jsx
+++ src/pages/reviews/ReviewList.jsx
@@ -31,6 +31,30 @@ function PageReviewList() {
       });
   };
 
+  const deleteReview = (deletingReview) => {
+    const { id: deletingReviewId } = deletingReview;
+    const url = `http://localhost:8000/shop/api/reviews/${deletingReviewId}/`;
+
+    setLoading(true);
+    setError(null);
+
+    Axios.delete(url)
+      .then(() => {
+        console.log('삭제 성공');
+        // 선택지 #1) 삭제된 항목만 상탯값에서 제거
+        setReviewList((prevReviewList) =>
+          prevReviewList.filter((review) => review.id !== deletingReviewId),
+        );
+        // 선택지 #2) 전체를 새로고침
+      })
+      .catch((error) => {
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
   return (
     <div>
       <h2>Review List</h2>
@@ -47,7 +71,11 @@ function PageReviewList() {
 
       <div className="">
         {reviewList.map((review) => (
-          <Review key={review.id} review={review} />
+          <Review
+            key={review.id}
+            review={review}
+            handleDelete={() => deleteReview(review)}
+          />
         ))}
       </div>
 
