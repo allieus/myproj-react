bb5a0731e292e0c7a0f38a5bc53009414ffdd722
Chinseok Lee <me@askcompany.kr>
2022-01-11 13:21:31 +0900

    디지털 시계에 년/월/일/요일 정보를 추가

--- src/pages/examples/Clock.jsx
+++ src/pages/examples/Clock.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import './Clock.css';
 
+const WEEKDAYS = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+
 function Clock() {
   const [date, setDate] = useState(new Date());
 
@@ -23,8 +25,14 @@ function Clock() {
       <h2>시계</h2>
 
       <div class="clock">
-        <p class="date">2021-10-05 TUE</p>
-        <p class="time">{date.toISOString().slice(11, 19)}</p>
+        <p class="date">
+          {date.getFullYear()}-{date.getMonth() + 1}-{date.getDate()}{' '}
+          {WEEKDAYS[date.getDay()]}
+        </p>
+        {/* <p class="time">{date.toLocaleTimeString('en-us')}</p> */}
+        <p class="time">
+          {date.getHours()}:{date.getMinutes()}:{date.getSeconds()}
+        </p>
         <p class="text">Powered by React.js</p>
       </div>
     </div>
