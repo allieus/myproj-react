bd7514e21e3aa69b12a32b2d2a309ca05e4d6c12
Chinseok Lee <me@askcompany.kr>
2022-01-12 21:49:16 +0900

    added ErrorBoundary 컴포넌트

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import ErrorBoundary from 'ErrorBoundary';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById('root'),
 );
