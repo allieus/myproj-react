200d94f4121947714b51e74ec2e72fe0c1f63df7
Chinseok Lee <me@askcompany.kr>
2022-01-13 21:27:08 +0900

    formData 계산을 useFieldValues 내에서 처리

--- src/components/news/ArticleForm.jsx
+++ src/components/news/ArticleForm.jsx
@@ -38,41 +38,11 @@ function ArticleForm({ articleId, handleDidSave }) {
     { manual: true },
   );
 
-  const { fieldValues, handleFieldChange, setFieldValues } = useFieldValues(
-    article || INIT_FIELD_VALUES,
-  );
+  const { fieldValues, handleFieldChange, setFieldValues, formData } =
+    useFieldValues(article || INIT_FIELD_VALUES);
 
+  // article 조회 시에 photo 속성을 빈 문자열로 변경
   useEffect(() => {
-    // 서버로 photo=null이 전달이 되면, 아래 오류가 발생
-    //   - The submitted data was not a file. Check the encoding type on the form.
-    //   - 대응 : fieldValues에서 photo만 제거해주거나, photo=null이라면 빈 문자열로 변경
-    // setFieldValues((prevFieldValues) => ({
-    //   ...prevFieldValues,
-    //   photo: '',
-    // }));
-
-    // 인자 1개를 받는 함수를 리턴 : 원본
-    // 함수(원본) => 변경된 사본을 리턴;
-    setFieldValues(
-      produce((draft) => {
-        draft.photo = '';
-      }),
-    );
-
-    // immer 2단계
-    // setFieldValues((prevFieldValues) => {
-    //   return produce(prevFieldValues, (draft) => {
-    //     draft.photo = '';
-    //   });
-
-    // immer 3단계
-    // setFieldValues((prevFieldValues) =>
-    //   produce(prevFieldValues, (draft) => {
-    //     draft.photo = '';
-    //   }),
-    // );
-
-    // immer 4단계
     setFieldValues(
       produce((draft) => {
         draft.photo = '';
@@ -82,18 +52,6 @@ function ArticleForm({ articleId, handleDidSave }) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    // fieldValues : 객체 (except 파일)
-    // 파일을 업로드할려면, FormData 인스턴스를 써야합니다.
-    const formData = new FormData();
-    Object.entries(fieldValues).forEach(([name, value]) => {
-      if (Array.isArray(value)) {
-        const fileList = value;
-        fileList.forEach((file) => formData.append(name, file));
-      } else {
-        formData.append(name, value);
-      }
-    });
 
     saveRequest({
       data: formData,
