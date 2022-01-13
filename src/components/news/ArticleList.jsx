9c8e0165f04448090547d7b7a5bdfb53e1fd5bb8
Chinseok Lee <me@askcompany.kr>
2022-01-13 13:32:33 +0900

    tailwind transition 예시 추가

--- src/components/news/ArticleList.jsx
+++ src/components/news/ArticleList.jsx
@@ -13,13 +13,16 @@ function ArticleList() {
   }, []);
 
   return (
-    <div className="my-5">
+    <div className="my-5 ">
       {loading && '로딩 중 ...'}
       {error && '로딩 중 에러가 발생했습니다.'}
       {articleList && (
         <div className="flex flex-wrap">
           {articleList.map((article) => (
-            <div key={article.id} className="w-full md:w-1/2 xl:w-1/3 px-4">
+            <div
+              key={article.id}
+              className="w-full md:w-1/2 xl:w-1/3 px-4 transition-transform hover:-translate-y-5 duration-300"
+            >
               <ArticleSummary article={article} />
             </div>
           ))}
