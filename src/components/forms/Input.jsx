43045a47d7b32c514ddea5169815c6e9c11f8af9
Chinseok Lee <me@askcompany.kr>
2022-01-15 10:20:43 +0900

    input[type=file]에서 선택된 이미지의 Preview이미지를 보여주도록 개선

--- src/components/forms/Input.jsx
+++ src/components/forms/Input.jsx
@@ -1,15 +1,26 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import FieldErrorMessages from './FieldErrorMessages';
 
 function Input({ type, name, value, placeholder, onChange, errorMessages }) {
   const [imageUrl, setImageUrl] = useState(value);
+  const inputRef = useRef();
 
   useEffect(() => {
     if (type === 'file') {
       setImageUrl((prevImageUrl) => {
-        return !value ? prevImageUrl : value;
+        return value || prevImageUrl;
       });
+
+      // "파일 선택"을 통해서 선택된 이미지를 보여줍니다.
+      const file = inputRef.current.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = function () {
+          setImageUrl(this.result); // base64 image
+        };
+        reader.readAsDataURL(file);
+      }
     }
   }, [type, value]);
 
@@ -36,9 +47,11 @@ function Input({ type, name, value, placeholder, onChange, errorMessages }) {
           </a>
         )}
         <input
+          ref={inputRef}
           type={type}
           name={name}
-          value={type !== 'file' ? value : ''}
+          // type=file 에서는 value 지정을 하지 않고, type=file 자체가 가지고 있는 값을 그대로 보여주도록 합니다.
+          value={type !== 'file' ? value : undefined}
           placeholder={placeholder}
           onChange={onChange}
           className={`p-2 outline-none focus:border ${
